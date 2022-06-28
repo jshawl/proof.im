46add3c9270a3e04fac6998f2fdebaba93a69acb
Jesse Shawl <jesse@jesse.sh>
2022-06-27 21:02:38 -0500

    wip

---
+++ spec/features/login_out_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Logging In' do
+  before do
+    @handle = Handle.create(name: 'jshawl')
+    @key = @handle.keys.create(content: 'RWQVeYdkyHjdHNLkbGPUmaD1rn4Il43FUsIwos6raMWg0NC4AqGgejkA')
+  end
+  it 'logs in and out' do
+    visit new_session_path
+    allow(SecureRandom).to receive(:uuid).and_return('abc-123')
+    visit new_session_path(handle: 'jshawl')
+
+    # server side
+    pr = Proof.create(
+      key: @key,
+      claim: File.read("spec/fixtures/session.txt"),
+      content: File.read("spec/fixtures/session.txt.minisig")
+    )
+
+    # client side
+    click_on "I did this"
+    expect(page).to have_content("Log Out jshawl")
+
+    click_on "Log Out jshawl"
+    expect(page).to have_content("Log In")
+  end
+end
