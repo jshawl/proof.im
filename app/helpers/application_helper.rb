45fe6150f2d94b197a6759c56604b5724d0e8abb
Jesse Shawl <jesse@jesse.sh>
2022-06-28 08:32:03 -0500

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return nil unless session['proven_claim']
     session['proven_claim'].split(":")[0]
   end
+
+  def handle_key_claim(handle, key)
+    "I am proving that I am #{handle} on proof.im with the following public key:\n#{key}"
+  end
 end
