eae2993ba1d106962f1b7626c9f635879cd86c5a
Jesse Shawl <jesse@jesse.sh>
2022-06-28 08:23:53 -0500

    bump coverage

--- app/controllers/proofs_controller.rb
+++ app/controllers/proofs_controller.rb
@@ -3,7 +3,7 @@ class ProofsController < ApplicationController
     @handle = Handle.find_by_name(params[:handle_id])
     @key = @handle.keys.find(params[:key_id])
     @claim = "I am proving that I am #{@handle.name} on proof.im with the following public key:
-#{@key.content}\n"
+#{@key.content}"
     @proof = @key.create_proof(proof_params.merge(claim: @claim))
     redirect_to handle_key_proof_path(@handle.name, @key)
   end
