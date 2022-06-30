a4577d6db65dff7265c1219a5e3fdbe0ebde2cb2
Jesse Shawl <jesse@jesse.sh>
2022-06-30 12:56:26 -0500

    support non-rsa ssh keys (#3)

--- app/models/proof.rb
+++ app/models/proof.rb
@@ -2,13 +2,13 @@ class Proof < ApplicationRecord
   belongs_to :key # eventually polymorphic
 
   def verification
-    if key.kind == "ssh-rsa"
+    if key.kind == "minisign"
+      pk = Minisign::PublicKey.new(key.content)
+      pk.verify(Minisign::Signature.new(signature), claim)
+    else
       pk = SSHData::PublicKey.parse_openssh(key.content)
       sig = SSHData::Signature.parse_pem(signature)
       pk.fingerprint == sig.public_key.fingerprint && sig.verify(claim)
-    else
-      pk = Minisign::PublicKey.new(key.content)
-      pk.verify(Minisign::Signature.new(signature), claim)
     end
   end
   def verified?
