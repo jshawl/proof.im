2e6d11776aa1198099681427e04b4f3a8f7b890d
Jesse Shawl <jesse@jesse.sh>
2022-06-30 12:43:17 -0500

    add support for ssh keys (#2)
    * add tests for ssh keys
    
    * ui enhancements for ssh key support
    
    * increase test coverage
    
    * more open instructions

--- app/models/key.rb
+++ app/models/key.rb
@@ -3,9 +3,24 @@ class Key < ApplicationRecord
   has_one :proof
   validates :content, presence: true
 
+  before_save :set_kind
+
   scope :verified, -> {includes(:proof).where.not({proof: {id: nil}})}
 
   def key_id
-    Minisign::PublicKey.new(content).key_id
+    if kind&.match(/ssh/)
+      SSHData::PublicKey.parse_openssh(content).fingerprint
+    else
+      Minisign::PublicKey.new(content).key_id
+    end
+  end
+
+  private
+  def set_kind
+    if self.content.match(/^ssh-/)
+      self.kind = self.content.split(" ")[0]
+    else
+      self.kind = 'minisign'
+    end
   end
 end
--- app/models/proof.rb
+++ app/models/proof.rb
@@ -2,8 +2,14 @@ class Proof < ApplicationRecord
   belongs_to :key # eventually polymorphic
 
   def verification
-    pk = Minisign::PublicKey.new(key.content)
-    pk.verify(Minisign::Signature.new(signature), claim)
+    if key.kind == "ssh-rsa"
+      pk = SSHData::PublicKey.parse_openssh(key.content)
+      sig = SSHData::Signature.parse_pem(signature)
+      pk.fingerprint == sig.public_key.fingerprint && sig.verify(claim)
+    else
+      pk = Minisign::PublicKey.new(key.content)
+      pk.verify(Minisign::Signature.new(signature), claim)
+    end
   end
   def verified?
     !!verification
